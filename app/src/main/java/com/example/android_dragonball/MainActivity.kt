005f2a3474512a902f6bc4ff76addc343d15b900
marcomadv <imarcoma@gmail.com>
2023-12-15 20:40:26 +0100

    setobservers con estados

--- app/src/main/java/com/example/android_dragonball/MainActivity.kt
+++ app/src/main/java/com/example/android_dragonball/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.android_dragonball
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.android_dragonball.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +18,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setListeners()
+        setObservers()
     }
 
     private fun setListeners() {
         binding.buttonLogin.setOnClickListener {viewModel.launchLogin(binding.editTextUser.toString(), binding.editTextPassword.toString())}
+    }
+
+    private fun setObservers(){
+        lifecycleScope.launch(Dispatchers.Main) {
+            viewModel.uiState.collect { state ->
+                when(state) {
+                    is MainActivityViewModel.State.Idle -> idle()
+                    is MainActivityViewModel.State.Error -> showError(state.message)
+                    is MainActivityViewModel.State.Loading -> showLoading(true)
+                    is MainActivityViewModel.State.SuccessLogin -> showSuccessLogin()
+                    is MainActivityViewModel.State.SuccessGetHeroes -> showSuccessGetHeroes(state.heroList)
+                }
+            }
+        }
     }
 }
