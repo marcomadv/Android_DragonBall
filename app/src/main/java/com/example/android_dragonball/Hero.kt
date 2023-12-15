4a6f643bf0be26eb69aa493245e92b87a8facdaa
marcomadv <imarcoma@gmail.com>
2023-12-15 20:18:58 +0100

    modelos, herodto + hero personalizado

---
+++ app/src/main/java/com/example/android_dragonball/Hero.kt
@@ -0,0 +1,7 @@
+package com.example.android_dragonball
+
+data class Hero(
+    val name: String,
+    val photo: String,
+    val life: Int = 100
+)
---
+++ app/src/main/java/com/example/android_dragonball/HeroDto.kt
@@ -0,0 +1,9 @@
+package com.example.android_dragonball
+
+data class HeroDto (
+    val name: String,
+    val photo: String,
+    val description: String,
+    val favorite: Boolean,
+    val id: String
+)
